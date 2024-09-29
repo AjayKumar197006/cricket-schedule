32f6def326b5cc9725ab8601e7e459d30e3dde71
AjayKumar197006 <ajaykumar197006@outlook.com>
2024-09-29 12:04:26 +0530

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -3,7 +3,7 @@ const db=require("./db")
 
 const app=express()
 app.use(express.json())
-app.listen(https://cricket-schedule.up.railway.app,()=>
+app.listen("https://cricket-schedule.up.railway.app",()=>
     {
         console.log("Server Started")
     })
